3d9d3ab3a14ce286b1b320bd34c7a4fab886f83c
Stephen-Hallett <stevohallett@gmail.com>
2025-06-14 22:36:30 +1200

    chore: Remove unddeeded files

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from tickers import TickerDetector
-
-ticker_detector = TickerDetector(os.environ["OPENAI_API_KEY"])
-
-print(ticker_detector.detect("National Grid PLC Ord", "Great Britain"))
